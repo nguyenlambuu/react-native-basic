d76aa1628708332030e31efa77677db20c7d8029
buu <nguyenlambuu@gmail.com>
2020-06-02 08:09:00 +0700

    Food app

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
+import useResults from '../hooks/useResults.js';
+
+const SearchScreen = ({ navigation }) => {
+	const [term, setTerm] = useState('');
+	const [searchApi, results, errorMessage] = useResults();
+
+	const filterResultsByPrice = (price) => {
+		// price === '$' || '$$' || '$$$'
+		return results.filter((result) => result.price === price);
+	};
+
+	return (
+		<>
+			<SearchBar
+				term={term}
+				onTermChange={setTerm}
+				onTermSubmit={() => searchApi(term)}
+			/>
+			{/* <Text>We have found {results.length} results</Text> */}
+			{errorMessage ? <Text>{errorMessage}</Text> : null}
+			<ScrollView>
+				<ResultsList
+					results={filterResultsByPrice('$')}
+					title="Cost Effective"
+				/>
+				<ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+				<ResultsList
+					results={filterResultsByPrice('$$$')}
+					title="Big Spender"
+				/>
+			</ScrollView>
+		</>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
